b72f276a4eaab007232e658d701674f63c96c46c
rony <vegappunk@gmail.com>
2026-04-23 09:46:47 +0700

    fix: mason

--- bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/data/repositories/{{name.snakeCase()}}_repository_impl.dart
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/data/repositories/{{name.snakeCase()}}_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:injectable/injectable.dart';
-import 'package:app/features/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart';
-import 'package:app/features/{{name.snakeCase()}}/data/data_sources/{{name.snakeCase()}}_remote_data_source.dart';
+import 'package:flutter_iiko_app/features/{{name.snakeCase()}}/domain/repositories/{{name.snakeCase()}}_repository.dart';
+import 'package:flutter_iiko_app/features/{{name.snakeCase()}}/data/data_sources/{{name.snakeCase()}}_remote_data_source.dart';
 
 @LazySingleton(as: {{name.pascalCase()}}Repository)
 class {{name.pascalCase()}}RepositoryImpl implements {{name.pascalCase()}}Repository {
